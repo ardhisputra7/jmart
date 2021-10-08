dbc130a6b8b1eaabfad0277f46b491feade754ca
unknown <ardhisputra7@gmail.com>
2021-10-08 13:51:36 +0700

    gantidevice

--- ArdhiJmartBO/Jmart.java
+++ ArdhiJmartBO/Jmart.java
@@ -5,7 +5,7 @@ public class Jmart
     
     public static void main(String[] args)
     {    
-        System.out.print("Hello from Eclipse");
+        System.out.print("Hello from Eclipse 2");
     }
     /*
     public static Product create()
