564e9e9c06e305fe7390e45d399614282f31bb86
unknown <ardhisputra7@gmail.com>
2021-10-11 20:54:04 +0700

    modul5ArdhiPT

--- ArdhiJmartBO/Algorithm.java
+++ ArdhiJmartBO/Algorithm.java
@@ -10,6 +10,41 @@ public class Algorithm
 		
 	}
 	
+	public static <T> List<T> collect (T[] array , T value) 
+	{
+		final Iterator <T> x = Arrays.stream(array).iterator();
+		return collect(x,value);
+	}
+	
+	public static <T> List<T> collect (Iterable<T> iterable , T value) 
+	{
+		final Iterator <T> x = iterable.iterator();
+		return collect(x,value);
+	}
+	
+	public static <T> List<T> collect (Iterator<T> iterator , T value) 
+	{
+		final Predicate <T> pred = value::equals;
+		return collect(iterator,pred);
+	}
+	
+	public static <T> List<T> collect (T[] array , Predicate<T> pred) 
+	{
+		final Iterator <T> x = Arrays.stream(array).iterator();
+		return collect(x,pred);
+	}
+	
+	public static <T> List<T> collect (Iterable<T> iterable , Predicate<T> pred) 
+	{
+		final Iterator <T> x = iterable.iterator();
+		return collect(x,pred);
+	}
+	
+	public static <T> List<T> collect (Iterator<T> iterator , Predicate<T> pred) 
+	{
+		return collect(iterator,pred);
+	}
+	
 	public static <T> int count (T[] array , T value)
 	{
 		final Iterator <T> x = Arrays.stream(array).iterator();
@@ -115,13 +150,83 @@ public class Algorithm
 		return find(iterator,pred);
 	}
 	
-	public static <T> T max (T first , T second)
+	public static <T extends Comparable<? super T>> T max (T first , T second)
 	{
 		return max(first,second);
 	}
 	
-	public static <T> T min (T first , T second)
+	public static <T extends Comparable<? super T>> T max (T[] array)
+	{
+		return Collections.max(Arrays.asList(array));
+	}
+	
+	public static <T extends Comparable<? super T>> T max (Iterable<T> iterable)
+	{
+		return null;
+	}
+	
+	public static <T extends Comparable<? super T>> T max (Iterator <T> iterator)
+	{
+		return null;
+	}
+	
+	public static <T> T max (T first , T second, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T max (T[] array, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T max (Iterable<T> iterable, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T max (Iterator <T> iterator, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T extends Comparable<? super T>> T min (T first , T second)
 	{
 		return min(first,second);
+	}
+	
+	public static <T extends Comparable<? super T>> T min (T[] array)
+	{
+		return Collections.min(Arrays.asList(array));
+	}
+	
+	public static <T extends Comparable<? super T>> T min (Iterable<T> iterable)
+	{
+		return null;
+	}
+	
+	public static <T extends Comparable<? super T>> T min (Iterator <T> iterator)
+	{
+		return null;
+	}
+	
+	public static <T> T min (T first , T second, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T min (T[] array, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T min (Iterable<T> iterable, Comparator<? super T> comparator)
+	{
+		return null;
+	}
+	
+	public static <T> T min (Iterator <T> iterator, Comparator<? super T> comparator)
+	{
+		return null;
 	}
 }
